1f85e07a04119d5741eefe2ef1aebfcc6a3cb172
heme90 <dbsrl0423@gmail.com>
2019-07-16 17:21:06 +0900

    no message

--- sample.js
+++ sample.js
@@ -1,27 +1,32 @@
 var http = require('http'); // Import Node.js core module
 var fs = require('fs');
+var url = require('url');
 var server = http.createServer(function (req, res) {   //create web server
-    
-    
+    var _url = req.url;
+    var qurey = url.parse(_url,true).query;
     if (req.url == '/') { //check the URL of the current request
         
         // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
         // set response content    
-        res.write(fs.readFileSync('C:\\MyNode\\nodetest\\index.html','utf-8'));
-        res.end();
+        fs.readFile(__dirname + '/index.html', (err, data) => { // 파일 읽는 메소드
+            if (err) {
+              return console.error(err); // 에러 발생시 에러 기록하고 종료
+            }
+            res.end(data, 'utf-8'); // 브라우저로 전송
+          });
     
     }
     else if (req.url == "/comment") {
         
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
         res.write('<html><h1>지도 주석 페이지의 html입니다</h1><br/></html>','utf-8');
         res.end();
     
     }else if (_url = '/comment.do' ) {
-        
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
         res.write('<h1>지도 주석 페이지의 실행 스크립트입니다</h1><br/>'
-        + req['comm']+ ' 을 주석으로 등록합니다','utf-8');
+        + qurey.comm + ' 을 주석으로 등록합니다','utf-8');
     
     }
     else{
